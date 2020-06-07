6b6ea4838ed0c257d92f080059ff59fd1d59fd07
acharyarajasekhar <acharya.r@live.com>
2020-06-08 02:32:07 +0530

    intro to push FCM

--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -2,6 +2,16 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { environment } from 'src/environments/environment';
 import { Router, RouterEvent } from '@angular/router';
+import { ToastService } from '@acharyarajasekhar/ngx-utility-services';
+
+import {
+  Plugins,
+  PushNotification,
+  PushNotificationToken,
+  PushNotificationActionPerformed
+} from '@capacitor/core';
+
+const { PushNotifications } = Plugins;
 
 @Component({
   selector: 'app-tabs',
@@ -19,18 +29,59 @@ export class TabsComponent implements OnInit {
 
   constructor(
     private platform: Platform,
-    private router: Router
+    private router: Router,
+    private toast: ToastService
   ) { }
 
   ngOnInit(): void {
     if (this.platform.is('android') || this.platform.is('ios')) {
       this.isMobileView = true;
+      this.initializePushNotifications();
     }
     this.router.events.subscribe((event: RouterEvent) => {
       if (!!event.id) {
         this.activePath = event.url;
       }
     })
+  }
+
+  initializePushNotifications() {
+    // Request permission to use push notifications
+    // iOS will prompt user and return if they granted permission or not
+    // Android will just grant without prompting
+    PushNotifications.requestPermission().then(result => {
+      if (result.granted) {
+        // Register with Apple / Google to receive push via APNS/FCM
+        PushNotifications.register();
+      } else {
+        // Show some error
+        this.toast.error("Push Notifications - Permission Denied");
+      }
+    });
+
+    PushNotifications.addListener('registration',
+      (token: PushNotificationToken) => {
+        console.log('Push registration success, token: ' + token.value);
+      }
+    );
+
+    PushNotifications.addListener('registrationError',
+      (error: any) => {
+        alert('Error on registration: ' + JSON.stringify(error));
+      }
+    );
+
+    PushNotifications.addListener('pushNotificationReceived',
+      (notification: PushNotification) => {
+        alert('Push received: ' + JSON.stringify(notification));
+      }
+    );
+
+    PushNotifications.addListener('pushNotificationActionPerformed',
+      (notification: PushNotificationActionPerformed) => {
+        alert('Push action performed: ' + JSON.stringify(notification));
+      }
+    );
   }
 
   selectMe(url) {
