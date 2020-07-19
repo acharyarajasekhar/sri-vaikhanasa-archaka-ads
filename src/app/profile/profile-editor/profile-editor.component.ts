42e3211ae28ce29137443bf2bc5d64089819a1c4
acharyarajasekhar <acharya.r@live.com>
2020-07-19 14:04:27 +0530

    issue fixes

--- src/app/profile/profile-editor/profile-editor.component.ts
+++ src/app/profile/profile-editor/profile-editor.component.ts
@@ -1,17 +1,16 @@
-import { Component, OnInit, ViewChild, NgZone } from '@angular/core';
-import { FormGroup } from '@angular/forms';
-import { ControlBase, ControlsService } from '@acharyarajasekhar/dynamic-forms';
-import { ModalController } from '@ionic/angular';
-import { environment } from 'src/environments/environment';
-import { FormDataTranslationService } from 'src/app/services/form.data.translation.service';
+import { Component, OnInit, ViewChild, NgZone } from "@angular/core";
+import { FormGroup } from "@angular/forms";
+import { ControlBase, ControlsService } from "@acharyarajasekhar/dynamic-forms";
+import { ModalController } from "@ionic/angular";
+import { environment } from "src/environments/environment";
+import { FormDataTranslationService } from "src/app/services/form.data.translation.service";
 
 @Component({
-  templateUrl: './profile-editor.component.html',
-  styleUrls: ['./profile-editor.component.css']
+  templateUrl: "./profile-editor.component.html",
+  styleUrls: ["./profile-editor.component.css"],
 })
 export class ProfileEditorComponent implements OnInit {
-
-  @ViewChild('dynamicForm', { static: true }) dynamicForm: any;
+  @ViewChild("dynamicForm", { static: true }) dynamicForm: any;
 
   controls: ControlBase<any>[];
   form: FormGroup;
@@ -27,53 +26,57 @@ export class ProfileEditorComponent implements OnInit {
     private modalController: ModalController,
     private formDataTranslationService: FormDataTranslationService,
     private zone: NgZone
-  ) { }
+  ) {}
 
   ngOnInit() {
-
     this.initDynamicFormControls().then(() => {
       this.isReadyToRender = true;
       this.initFormGroup();
       if (!!this.profile && !!this.profile.id) {
         this.setInitialFormValues();
       }
-    })
-
+    });
   }
 
   private initDynamicFormControls() {
     return new Promise((res) => {
       this.formConfig = environment.formConfigs.profileEditForm;
-      this.formDataTranslationService.setFormConfigWithTranslatedText(this.formConfig)
-        .then((translatedFormConfig: any) => {          
+      this.formDataTranslationService
+        .setFormConfigWithTranslatedText(this.formConfig)
+        .then((translatedFormConfig: any) => {
           if (!!translatedFormConfig) {
             this.zone.run(() => {
-              this.controls = this.controlSvc.getControls(translatedFormConfig.controls);
-            })
+              this.controls = this.controlSvc.getControls(
+                translatedFormConfig.controls
+              );
+            });
           }
           res();
-        })
-    })
+        });
+    });
   }
 
   private initFormGroup() {
     this.form = new FormGroup({});
-    this.form.valueChanges.subscribe(val => { this.submitted = val; });
+    this.form.valueChanges.subscribe((val) => {
+      this.submitted = val;
+    });
   }
 
   private setInitialFormValues() {
     this.zone.run(() => {
-      this.controls.forEach(c => c.value = this.profile[c.name]);
-    })
+      this.controls.forEach((c) => (c.value = this.profile[c.name]));
+    });
   }
 
   cancel() {
-    this.modalController.dismiss(null, 'cancel');
+    this.modalController.dismiss(null, "cancel");
   }
 
   private handleUndefined = (obj) => {
-    Object.keys(obj).forEach(key => {
-      if (obj[key] && typeof obj[key] === 'object') this.handleUndefined(obj[key]);
+    Object.keys(obj).forEach((key) => {
+      if (obj[key] && typeof obj[key] === "object")
+        this.handleUndefined(obj[key]);
       else if (obj[key] === undefined) obj[key] = null;
     });
     return obj;
@@ -81,12 +84,14 @@ export class ProfileEditorComponent implements OnInit {
 
   onSave() {
     if (this.form.invalid) {
-      this.dynamicForm.showErrors();
-    }
-    else {
+      if (!!this.dynamicForm) {
+        this.dynamicForm.showErrors();
+      } else {
+        console.log("TODO: How dynamic form is null or undefined...");
+      }
+    } else {
       this.handleUndefined(this.submitted);
-      this.modalController.dismiss(this.submitted, 'ok');
+      this.modalController.dismiss(this.submitted, "ok");
     }
   }
-
 }
